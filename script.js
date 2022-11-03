99f575eb307d51e1f783a132c00846483ed0132c
Faiz Un Nisa <memonfaiz20@gmail.com>
2022-11-04 03:13:51 +0500

    Add the calculatorBill() function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ There are three function
 const billInput = document.getElementById('billTotalInput')
 const tipInput = document.getElementById('tipInput')
 const numberOfPeopleDiv = document.getElementById('numberOfPeople')
-const perPersonalTotal = document.getElementById('perPersonTotal')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
 
 // Get number of people from number of people div
 let numberOfPeople = Number(numberOfPeopleDiv.innerText)
@@ -18,4 +18,20 @@ let numberOfPeople = Number(numberOfPeopleDiv.innerText)
 // Calculator the total bill per person
 const calculateBill = () => {
     // get bill from user input & convert it into a number
+    const bill = Number(billInput.value)
+
+    // get the tip from the user input and convert it into percentage
+    const tipPercentage = Number(tipInput.value) / 100
+
+    // get the total tip amount
+    const tipAmount = bill * tipPercentage
+
+    // calculate the total
+    const totalAmount = tipAmount + bill
+
+    // calculate the per person total
+    const perPersonTotal = totalAmount / numberOfPeople
+
+    // Update the per person total and show it to DOM
+    perPersonTotalDiv.innerText = `$${ perPersonTotal.toFixed(2)}`
 }
